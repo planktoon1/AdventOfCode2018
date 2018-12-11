30a795da1aa134c007b8276ef9fce6168aa88a87
planktoon1 <fkoe90@gmail.com>
2018-12-11 15:01:38 +0100

    3. December part two

--- 3.December/parttwo.js
+++ 3.December/parttwo.js
@@ -1,28 +1,52 @@
 const fs = require('fs').promises;
 
-
 const app = async () => {
     const text = await fs.readFile('./input.txt', 'utf8');
-
     const trimmedText = text.split(/[\r\n]+/);
 
-    let linesWithDublicates = 0;
+    let fabric = [];
+    for (let i = 0; i < 1000; i++) {
+        fabric[i] = [];
 
-    for (i = 0; i < trimmedText.length; i++) {
+        for (let j = 0; j < 1000; j++) {            
+            fabric[i][j] = '.';
+        }
         
+    }
+    
+    let overlapCount = 0;
+    let ids = ['the false id: 0'];
+    for (let i = 1; i < trimmedText.length +1; i++) {
+        ids.push(i);       
+    }
+    
+    for (rectangle of trimmedText) {
+        const info = rectangle.split(' ');
 
-        for (j = i+1; j < trimmedText.length; j++) {
+        const leftOffset = parseInt(info[2].split(',')[0]);
+        const topOffset = parseInt(info[2].split(',')[1].replace(':',''));
+
+        const width = parseInt(info[3].split('x')[0]);
+        const height = parseInt(info[3].split('x')[1]);
+
+        const id = parseInt(info[0].replace('#',''));
+        //console.log(`left: ${leftOffset}, top: ${topOffset} | {${width}, ${height}}`);
+
+        for (let x = leftOffset; x < (leftOffset + width); x++) {
             
-            let oneCharDiffrence = false;
-            for(c = 0; c < trimmedText[i].length; c++) {
-                const c1 = trimmedText[i].charAt(c);
-                const c2 = trimmedText[j].charAt(c)
-                if      (c1 !== c2 && !oneCharDiffrence) oneCharDiffrence = true;
-                else if (c1 !== c2 &&  oneCharDiffrence) {oneCharDiffrence = false; break;};
+            for (let y = topOffset; y < (topOffset + height); y++) {
+                if (fabric[x][y] === '.') {fabric[x][y] = `${id}`;}
+                else { //If its an overlab. Remove the id's from the list of not overlabbed ids
+                    const otherId = fabric[x][y];
+                    ids.splice(otherId, 1, 'removed');
+                    ids.splice(id, 1, 'removed');
+                }
             }
-            if (oneCharDiffrence) console.log(`${trimmedText[i]} | ${trimmedText[j]}`);
         }
     }
+
+    const remainingId = ids.filter((e) => {return e !== 'removed'});
+    console.log(remainingId);
 
 }
 
