1c1936d0d071c37ad8cbbe0ece124046c462a070
planktoon1 <fkoe90@gmail.com>
2018-12-13 19:29:27 +0100

    4. December part two solved

--- 4.December/parttwo.js
+++ 4.December/parttwo.js
@@ -2,52 +2,93 @@ const fs = require('fs').promises;
 
 const app = async () => {
     const text = await fs.readFile('./input.txt', 'utf8');
-    const trimmedText = text.split(/[\r\n]+/);
+    const logEntriesText = text.split(/[\r\n]+/);
+    
+    // Make the data into JS objects 
+    const logEntries = [];
+    for (logEntry of logEntriesText) {
+        let type = '';
+        if      (logEntry.includes('Guard'))        { type = 'shift start'}
+        else if (logEntry.includes('falls asleep')) { type = 'sleep start'}
+        else                                        { type = 'sleep end'}
 
-    let fabric = [];
-    for (let i = 0; i < 1000; i++) {
-        fabric[i] = [];
+        let id = 0;
+        if (type === 'shift start') { id = parseInt(logEntry.split(' ')[3].replace('#', ''))}
 
-        for (let j = 0; j < 1000; j++) {            
-            fabric[i][j] = '.';
+        let date = new Date(logEntry.split(']')[0].replace('[','') + 'Z');
+        let minute = parseInt(logEntry.substring(15,17));
+
+        const entry = {
+            date: date,
+            type: type,
+            id: id,
+            minute: minute
+        }
+        logEntries.push( entry );
+    }
+
+    // Sort entries
+    logEntries.sort( (a, b) => {return a.date - b.date});
+    
+    // Make list of guards, and add guard id to all log entries
+    const guards = [];
+    let currentGuard = null;
+    let startSleepTime = null;
+        for (logEntry of logEntries) {
+        if (logEntry.type === 'shift start') {
+            currentGuard = guards.find( (g) => {return g.id === logEntry.id});
+            if (!currentGuard) { guards.push(currentGuard = { id: logEntry.id, sleepTime: 0 }); };
+        } else {logEntry.id = currentGuard.id;}
+
+        if (logEntry.type === 'sleep start') {
+            startSleepTime = logEntry.minute;
         }
         
+        if (logEntry.type === 'sleep end') {
+            const minutesAsleep = logEntry.minute - startSleepTime - 1;
+            currentGuard.sleepTime = currentGuard.sleepTime + minutesAsleep;
+            logEntry.sleepStarted = startSleepTime; 
+        }
+
     }
     
-    let overlapCount = 0;
-    let ids = ['the false id: 0'];
-    for (let i = 1; i < trimmedText.length +1; i++) {
-        ids.push(i);       
-    }
-    
-    for (rectangle of trimmedText) {
-        const info = rectangle.split(' ');
+    // Use same technique as part one, but do it to all guards.
+    const guardsSleepLogs = [];
+    for (guard of guards) {
+        const sleepersLogs = logEntries.filter( (entry) => {return (entry.id === guard.id && entry.type === 'sleep end')});
 
-        const leftOffset = parseInt(info[2].split(',')[0]);
-        const topOffset = parseInt(info[2].split(',')[1].replace(':',''));
+        // Make a list of the minutes 00-59 and populate it with the slept minutes of the guard
+        const minutesAsleep = [];
+        for (entry of sleepersLogs) {
+            const startMinute = entry.sleepStarted;
+            const endMinute = entry.minute;
 
-        const width = parseInt(info[3].split('x')[0]);
-        const height = parseInt(info[3].split('x')[1]);
-
-        const id = parseInt(info[0].replace('#',''));
-        //console.log(`left: ${leftOffset}, top: ${topOffset} | {${width}, ${height}}`);
-
-        for (let x = leftOffset; x < (leftOffset + width); x++) {
-            
-            for (let y = topOffset; y < (topOffset + height); y++) {
-                if (fabric[x][y] === '.') {fabric[x][y] = `${id}`;}
-                else { //If its an overlab. Remove the id's from the list of not overlabbed ids
-                    const otherId = fabric[x][y];
-                    ids.splice(otherId, 1, 'removed');
-                    ids.splice(id, 1, 'removed');
-                }
+            for (let min = startMinute; min < endMinute; min++) {
+                if ( !minutesAsleep[min]) { minutesAsleep[min] = {minute: min, sleepingFor: 1};}
+                else minutesAsleep[min].sleepingFor = minutesAsleep[min].sleepingFor + 1;
             }
         }
+
+        // save info about the minute that the guard slept the most on
+        minutesAsleep.sort( (a,b) => {return b.sleepingFor - a.sleepingFor});
+        if (minutesAsleep.length > 0) {
+            // ST = sleep time
+            const guardSleepInfo = {
+                guard: guard.id,
+                toughMin: minutesAsleep[0].minute,
+                toughMinST: minutesAsleep[0].sleepingFor,
+                totalST: guard.sleepTime
+            };
+
+            guardsSleepLogs.push(guardSleepInfo);
+        } else { console.log(` Guard ${guard.id} didnt sleep at all `)}
+        
     }
 
-    const remainingId = ids.filter((e) => {return e !== 'removed'});
-    console.log(remainingId);
+    guardsSleepLogs.sort( (a, b) => {return b.toughMinST - a.toughMinST});
+    const winner = guardsSleepLogs[0];
+    console.log( winner);
+    console.log( `answer(id * minute) = ${winner.guard * winner.toughMin}`);
 
 }
-
 app();
